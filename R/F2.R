41ddc6c5bcb517a0376c7bb93f5b4c95b364939e
davidtorres18118 <david8521@hotmail.es>
2024-11-25 16:31:47 -0500

    7ma

---
+++ R/F2.R
@@ -0,0 +1,16 @@
+#' Función de ejmplo: operación aritmética RESTA
+#'
+#' @param x_1 Primer valor
+#' @param x_2 Segundo valor
+#'
+#' @return Retorna el resultado de la resta
+#' @export
+#'
+#' @examples
+#' F_example(x_1 = 3, x_2 = 4)
+#' F_example(x_1 = 1, x_2 = 2)
+F2 <- function(x_1, x_2){
+  aux_1 <- x_1 - x_2
+
+  return(aux_1)
+}
